fa7fc9f4c9ab5943d92420215df4334d5aa87eb0
Syaamil Faiq <syaamil1136@gmail.com>
2023-01-20 20:32:15 +0800

    responsive header

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {},
+        screens: {
+            "mobile-sm": "320px",
+            "mobile-md": "375px",
+            "mobile-lg": "425px",
+            sm: "640px",
+            md: "768px",
+            lg: "1024px",
+            xl: "1280px",
+            "2xl": "1536px",
+        },
+        colors: {
+            cultured: "#F6F6F6",
+            platinum: "#E8E8E8",
+            jet: "#333333",
+            "darkred": "#990100",
+            "ioe": "#E71D36",
+        },
+    },
+    plugins: [],
+};
